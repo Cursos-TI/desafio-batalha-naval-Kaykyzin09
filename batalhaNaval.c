3db92adb3aa93f7eb860b7316fcddbe0708e6b27
Kaykyzin09 <kaykygabrielpenha732@gmail.com>
2025-04-02 20:53:56 -0300

    BatalhaNaval.c
    Tema 4: Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,40 +2,43 @@
 
 int main () {
 //Declarando variável Array
-    char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};// variavel I
+    int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};// variavel J
     int tabuleiro[10][10] = {0};
-  
+
 //Navio 1 ( Horizontal )  I
-    tabuleiro [2][4] = 3;
-    tabuleiro [3][4] = 3;
-    tabuleiro [4][4] = 3;
-
+    for(int i = 6; i < 9; i++){
+    tabuleiro[i][3] = 3;
+}
 //Navio 2 ( Vertical )    J
-    tabuleiro [6][5] = 3;
-    tabuleiro [6][6] = 3;
-    tabuleiro [6][7] = 3;
-    
+    for(int i = 5; i < 8; i++){
+    tabuleiro [5][i] = 3;
+}
+//posição  do navio 3 ( diagonal )
+    for(int i =1;i < 4; i++){
+        tabuleiro[i][i] = 3;
+        for(int j = 0;j < 3; j++){ //navio 4 (diagonal)
+        tabuleiro [i][-i] = 3;
+        }
+    }
     printf("### BATALHA NAVAL ###\n\n");
-
-// loop coluna
-    printf("    ");
-    for(int i = 0;i < 10; i++)
+// coluna da variavel I
+printf("   ");
+    for (int i = 0; i < 10; i++)
     {
-        printf(" %c ", coluna[i]);
+        printf("%c  ", coluna[i]);
     }
     printf("\n");
-    
-// loop aninhado linha e tabuleiro
-    for(int j = 0;j < 10; j++)
+
+// linha da variavel J
+    for (int j = 0; j < 10; j++)
     {
-        printf(" %2d ", linha[j]);
-        for(int i = 0;i < 10; i++)
+        printf("%2d", linha[j]);
+        for (int i = 0; i < 10; i++)
         {
-            printf(" %d ", tabuleiro[i][j]);
+            printf(" %d ", tabuleiro[j][i]);
         }
         printf("\n");
     }
-    
    return 0;
 }
